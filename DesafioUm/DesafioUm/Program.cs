5db35b8b007744e34da6bcb2368dd22c92d9f83d
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-06-08 18:53:47 -0300

    Ingrid Caroline Guilherme

---
+++ DesafioUm/DesafioUm/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+Console.WriteLine("Digite o seu nome: ");
+string nomeDoUsuario = Console.ReadLine();
+Console.WriteLine("Digite a sua idade: ");
+int idadeDoUsuario = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite o ano atual: ");
+int anoAtual = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Olá {nomeDoUsuario}, você nasceu em {anoAtual - idadeDoUsuario}");
