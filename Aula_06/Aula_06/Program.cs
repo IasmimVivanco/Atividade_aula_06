30e6d0fda91c224c77e1f3ac5074f1746db38998
Sheylla Cristiane <sheyllacristiane@hotmail.com>
2022-06-08 20:01:32 -0300

    Subindo a atividade

---
+++ Aula_06/Aula_06/Program.cs
@@ -0,0 +1,14 @@
+﻿string nomeDoUsuario;
+int idadeDoUsuario;
+int anoAtual = 2022;
+int anoNascimento;
+
+Console.WriteLine("Digite o seu nome:");
+nomeDoUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite a sua idade:");
+idadeDoUsuario = Convert.ToInt32(Console.ReadLine());
+
+anoNascimento =  anoAtual - idadeDoUsuario;
+
+Console.WriteLine("Olá " + nomeDoUsuario + ", você nasceu em: " + anoNascimento);
