ee445a89f13712b7e6985efbcaae2252d67af8ac
Caroline-githubb <ingrid_caroliine@yahoo.com.br>
2022-06-08 19:16:11 -0300

    Adicionado o desafio 02

--- DesafioUm/DesafioUm/Program.cs
+++ DesafioUm/DesafioUm/Program.cs
@@ -8,3 +8,12 @@ Console.WriteLine("Digite o ano atual: ");
 int anoAtual = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine($"Olá {nomeDoUsuario}, você nasceu em {anoAtual - idadeDoUsuario}");
+
+
+//DESAFIO PROFESSOR
+Console.WriteLine("Digite o número em km que deseja fazer a conversão para milhas: ");
+double km = Convert.ToDouble(Console.ReadLine());
+double conversaoMilha = 0.62d;
+double milhas = km * conversaoMilha;
+
+Console.WriteLine($"Seu veículo percorreu um total de {milhas} milhas");
