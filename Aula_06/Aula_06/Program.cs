977772dfaaf1678a867b02275fe2f86c36bec0e9
IasmimVivanco <iasmim.vivanco@hotmail.com>
2022-06-08 18:44:22 -0300

    Iasmim Oliveira Vivanco

---
+++ Aula_06/Aula_06/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite seu nome:");
+string nomeDoUsuario = Console.ReadLine();
+
+Console.WriteLine("Digite sua Idade:");
+int idadeDoUsuario = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o ano atual:");
+int anoAtual = int.Parse(Console.ReadLine());
+
+int anoDeNascimento = anoAtual - idadeDoUsuario;
+
+Console.WriteLine($"Olá,{nomeDoUsuario} você nasceu em {anoDeNascimento}.");
