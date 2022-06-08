91f7e2f4ad8ae94acec8bf68645a8305aa70a3a2
Nathalia França <nathrfranca@gmail.com>
2022-06-08 19:55:58 -0300

    Atividade da aula 6

---
+++ aula06/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Insira seu nome");
+string nomeDeUsuario = Console.ReadLine(); 
+
+Console.WriteLine("Insira sua idade");
+int idadeDoUsuario = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Insira o ano atual");
+int anoAtual = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Olá {nomeDeUsuario}, você nasceu em {anoAtual - idadeDoUsuario}");
+
