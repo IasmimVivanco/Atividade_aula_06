c600e8f83e0fc84fe5e86fa58a3571011c4054ef
Sheylla Cristiane <sheyllacristiane@hotmail.com>
2022-06-08 21:20:02 -0300

    Desafio 2

--- Aula_06/Aula_06/Program.cs
+++ Aula_06/Aula_06/Program.cs
@@ -1,4 +1,5 @@
-﻿string nomeDoUsuario;
+﻿// Desafio 1
+string nomeDoUsuario;
 int idadeDoUsuario;
 int anoAtual = 2022;
 int anoNascimento;
@@ -9,6 +10,18 @@ nomeDoUsuario = Console.ReadLine();
 Console.WriteLine("Digite a sua idade:");
 idadeDoUsuario = Convert.ToInt32(Console.ReadLine());
 
-anoNascimento =  anoAtual - idadeDoUsuario;
+anoNascimento = anoAtual - idadeDoUsuario;
 
 Console.WriteLine("Olá " + nomeDoUsuario + ", você nasceu em: " + anoNascimento);
+
+// Desafio 2
+int quilometrosPercorridosPeloVeiculo;
+const float milhas = 0.62f;
+float totalMilhas;
+
+Console.WriteLine("Digite o seu quilometros Percorridos:");
+quilometrosPercorridosPeloVeiculo = Convert.ToInt32(Console.ReadLine());
+
+totalMilhas = quilometrosPercorridosPeloVeiculo * milhas;
+
+Console.WriteLine("Seu veículo percorreu um total de " + totalMilhas  + " milhas.");
